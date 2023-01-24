606dbc573fad61d2feccf9ae547e632b297a8f32
Davi Souza <davi.souza@grupohostweb.local>
2023-01-24 16:05:19 -0300

    atualizando

--- Artigos.sql
+++ Artigos.sql
@@ -6,4 +6,10 @@ Migrar o sql server
 
 https://learn.microsoft.com/pt-br/sql/sql-server/migrate/guides/sql-server-to-sql-server-upgrade-guide?view=sql-server-ver16
 -----------------------------------------------------
+
+-- SQL SERVER – Razões – Não é possível reduzir o log de transações
+
+https://blog.sqlauthority.com/2022/08/24/sql-server-reasons-not-able-to-shrink-the-transaction-log/
+
+-----------------------------------------------------
 https://imasters.com.br/banco-de-dados/sql-server-nolock-vs-readpast-voce-sabe-diferenca-entre-os-dois
