c087b946b70bc0cb9f550750b05ca1bdc03726f4
Davi Souza <davi.souza@grupohostweb.local>
2023-01-30 17:30:56 -0300

    atualizando

--- Dicas/Artigos.sql
+++ Dicas/Artigos.sql
@@ -1,15 +1,29 @@
 Formatar sql online - http://poorsql.com/
------------------------------------------------------
+------------------------------------------------------------------------------------------------
 /*
 Migrar o sql server
 */
 
 https://learn.microsoft.com/pt-br/sql/sql-server/migrate/guides/sql-server-to-sql-server-upgrade-guide?view=sql-server-ver16
------------------------------------------------------
+------------------------------------------------------------------------------------------------
 
 -- SQL SERVER – Razões – Não é possível reduzir o log de transações
 
 https://blog.sqlauthority.com/2022/08/24/sql-server-reasons-not-able-to-shrink-the-transaction-log/
 
------------------------------------------------------
-https://imasters.com.br/banco-de-dados/sql-server-nolock-vs-readpast-voce-sabe-diferenca-entre-os-dois
+------------------------------------------------------------------------------------------------
+
+/*
+SQL SERVER REPLICATION
+*/
+
+https://www.devmedia.com.br/introducao-a-sql-server-replication/6497
+
+------------------------------------------------------------------------------------------------
+
+/*
+SQL Server Replication Passo-a-Passo
+*/
+
+https://www.devmedia.com.br/sql-server-replication-passo-a-passo/6679
+------------------------------------------------------------------------------------------------
