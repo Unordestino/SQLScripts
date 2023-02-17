94a3911deb26d54b35294f49f372a3e93da0767d
Davi Souza <davi.souza@grupohostweb.local>
2023-02-17 18:00:04 -0300

    att

---
+++ Internals/tempo de worker em execução em estado SUSPENDED ou RUNNABLE.sql
@@ -0,0 +1,31 @@
+
+/*
+tempo de worker em execução em estado SUSPENDED ou RUNNABLE.
+Na saída, quando w_runnablee w_suspendedforem iguais, representa o tempo que o trabalhador está no estado SUSPENSO. 
+Caso contrário, w_runnable representa o tempo gasto em milissegundos pelo trabalhador no estado RUNNABLE.
+*/
+
+
+
+SELECT   
+    t1.session_id,  
+    CONVERT(varchar(10), t1.status) AS status,  
+    CONVERT(varchar(15), t1.command) AS command,  
+    CONVERT(varchar(10), t2.state) AS worker_state,  
+    w_suspended =   
+      CASE t2.wait_started_ms_ticks  
+        WHEN 0 THEN 0  
+        ELSE   
+          t3.ms_ticks - t2.wait_started_ms_ticks  
+      END,  
+    w_runnable =   
+      CASE t2.wait_resumed_ms_ticks  
+        WHEN 0 THEN 0  
+        ELSE   
+          t3.ms_ticks - t2.wait_resumed_ms_ticks  
+      END  
+  FROM sys.dm_exec_requests AS t1  
+  INNER JOIN sys.dm_os_workers AS t2  
+    ON t2.task_address = t1.task_address  
+  CROSS JOIN sys.dm_os_sys_info AS t3  
+  WHERE t1.scheduler_id IS NOT NULL;
