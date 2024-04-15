53b301d485fe27beb6508b2dc191b6bdb59afb4e
Davi Souza <davi.souza@grupohostweb.local>
2024-04-15 16:19:55 -0300

    att

--- Memória/Consumo de Memória por Base de Dados.sql
+++ Memória/Consumo de Memória por Base de Dados.sql
@@ -1,5 +1,21 @@
+-- Uso do bata cache por DB
+WITH AggregateBufferPoolUsage
+AS (SELECT DB_NAME(database_id) AS [Database Name],
+           CAST(COUNT(*) * 8 / 1024.0 AS DECIMAL(10, 2)) AS [CachedSize]
+    FROM sys.dm_os_buffer_descriptors WITH (NOLOCK)
+    WHERE database_id <> 32767 -- ResourceDB
+    GROUP BY DB_NAME(database_id))
+SELECT ROW_NUMBER() OVER (ORDER BY CachedSize DESC) AS [Buffer Pool Rank],
+       [Database Name],
+       CachedSize AS [Cached Size (MB)],
+       CAST(CachedSize / SUM(CachedSize) OVER () * 100.0 AS DECIMAL(5, 2)) AS [Buffer Pool Percent]
+FROM AggregateBufferPoolUsage
+ORDER BY [Buffer Pool Rank];
+GO
 
 
+
+------------------------
 --Memória por base de dados
 SELECT  CASE database_id
           WHEN 32767 THEN 'ResourceDb'
@@ -11,6 +27,18 @@ FROM    sys.dm_os_buffer_descriptors
 GROUP BY DB_NAME(database_id) ,
         database_id
 ORDER BY cached_pages_count DESC ;
+
+---------------------------------------Apenas uma
+USE master
+GO
+SELECT DB_NAME (database_id) AS [Database Name],
+CAST(COUNT(*)*8/1024.0 AS DECIMAL(10, 2)) AS [CachedSizeMB],
+COUNT(page_id) AS PageCount
+FROM sys.dm_os_buffer_descriptors WITH (NOLOCK)
+WHERE database_id IN (DB_ID('Northwind'))
+GROUP BY DB_NAME (database_id)
+GO
+
 
 
 /*
